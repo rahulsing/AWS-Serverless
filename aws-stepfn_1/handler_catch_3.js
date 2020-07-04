6948d362bf64a316500c3b1abc5efcbb4610fda5
Rahul <errahul289@gmail.com>
2020-07-04 00:39:29 -0500

    Added 5 more Topics

---
+++ aws-stepfn_1/handler_catch_3.js
@@ -0,0 +1,31 @@
+module.exports.add = async ({ x, y }) => {
+  return x + y
+}
+
+class NumberTooBig extends Error
+{
+  constructor(n)
+  {
+  super(`${n} is too big!`)
+  this.name='NumberTooBig'
+  Error.captureStackTrace(this, NumberTooBig)
+  }
+}
+
+module.exports.double = async ( n ) => {
+  if(n>50)
+  {
+    throw new NumberTooBig(n)
+  }
+  return  n * 2
+}
+
+
+
+module.exports.sample = async function(event, context) {
+  console.log('Remaining time: ', context.getRemainingTimeInMillis())
+  console.log('Function name: ', context.functionName)
+  console.log("EVENT: \n" + JSON.stringify(event, null, 2))
+  return context.logStreamName
+}
+
